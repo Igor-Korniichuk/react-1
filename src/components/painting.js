a108473e5923039438e0b806f6415a9a508142a9
Igor-Korniichuk <kiv160780@gmail.com>
2022-07-05 12:24:34 +0300

    React-1.2

--- src/components/painting.js
+++ src/components/painting.js
@@ -1,5 +1,8 @@
+import PropTypes from 'prop-types';
+
+
 export default function Painting(props) {
-  const {url, title, profileUrl, authorName, price} = props
+  const {url, title, profileUrl, authorName, price, quantity} = props
   return (<div>
       <img src={url} alt={title} width="480" />
       <h2>{title}</h2>
@@ -7,7 +10,17 @@ export default function Painting(props) {
       Автор: <a href={profileUrl}>{authorName}</a>
       </p>
     <p>Цена: {price} кредитов</p>
-      <p>Доступность: заканчивается или есть в наличии</p>
+      <p>Доступность: {quantity < 10 ? 'заканчивается' : 'есть в наличии'}</p>
       <button type="button">Добавить в корзину</button>
     </div>)
 }
+
+Painting.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  profileUrl: PropTypes.string.isRequired,
+  authorName: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+}
+
