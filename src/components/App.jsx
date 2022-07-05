4592c8160967e4fc8098bdbdbd314c4bc376ce34
Igor-Korniichuk <kiv160780@gmail.com>
2022-07-05 18:52:52 +0300

    react-1-styles-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import { PageTitle } from "components/PageTitle/PageTitle";
+import { EventBoard } from "components/EventBoard/EventBoard";
+import upcomingEvents from "upcoming-events"
 
 export const App = () => {
     return (<div>
-        <PageTitle />
+        <PageTitle text="24th Core Worlds Coalition Conference" />
+        <EventBoard events={ upcomingEvents } />
     </div>);
 }
